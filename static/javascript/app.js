5214dcf32cce390865fef0365cc340ff113c1898
aretberg <a.retberg@gmail.com>
2024-06-15 10:14:02 -0400

    api/3dpaws template
    added api/3dpaws route that will render a weather_data.json file
    
    updated app.js file to fetch the weather.json file from /api/3dpaws and update the html weather values

--- static/javascript/app.js
+++ static/javascript/app.js
@@ -2,15 +2,28 @@ const title = document.getElementById('main-heading');
 console.log(title);
 var weather_data = {};
 
-function print_weather_data(weather_data){
-    console.log("Temperature (F): " + weather_data.tempf);
+function update_weather_data(data){
+    const id_temp = document.getElementById('temperature');
+    id_temp.innerText = data.temperature; 
+
+    const id_humid = document.getElementById('humidity');
+    id_humid.innerText = data.humidity;
+
+    const id_pres = document.getElementById('pressure');
+    id_pres.innerText = data.pressure;
 }
 
-fetch("/weather_data")
+fetch("/api/3dpaws")
     .then(res => res.json())
     .then(data => {
-        print_weather_data(data);
-    });
+        update_weather_data(data);
+    })
+
+
+    // .then(res => res.json())
+    // .then(data => {
+    //     print_weather_data(data);
+    // });
 
 // async function get_paws_data() {
 //     const response = await fetch("/weather_data");
